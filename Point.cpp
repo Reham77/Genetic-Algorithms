8a68074cfa7d0f0e1455f465f0c1248a7c16dc84
Reham Tarek <rehamtarek666666@gmail.com>
2021-07-14 16:38:46 +0200

    Added Point class

---
+++ Point.cpp
@@ -0,0 +1,12 @@
+#include "Point.h"
+
+
+Point::Point(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+
+Point::Point() {
+    this->x = 0;
+    this->y = 0;
+}
---
+++ Point.h
@@ -0,0 +1,14 @@
+#ifndef UNTITLED7_POINT_H
+#define UNTITLED7_POINT_H
+
+class Point {
+public:
+    double x, y;
+
+    Point();
+
+    Point(double x, double y);
+};
+
+
+#endif //UNTITLED7_POINT_H
